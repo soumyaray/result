d8c26e77e2f2f54d710e99f763a9b9d49e2bed6f
Soumya Ray <soumya.ray@gmail.com>
2023-12-02 23:50:59 +0800

    test: print result monads

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,4 +1,8 @@
-test_that("HAPPY: result() creates result monad from an error-prone function", {
+test_that("HAPPY: can print success and failure results", {
   expect_output(print(success()), "ok: done")
   expect_output(print(failure()), "error: failed")
+})
+
+test_that("HAPPY: can print result monads", {
+  expect_output(print(result(mean)), "result monad")
 })
